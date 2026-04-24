75dc59aed6e9229a85de10b953ec2b78aeb9bdc5
Luca <44218525+LucaBonamino@users.noreply.github.com>
2026-04-24 13:19:03 +0200

    Include restricted ai (#3)
    * add extenal dependency
    
    * add restricted algebraic immunity and Boolean function object
    
    * workflow update

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-mod vandermonde;
 mod ai;
+mod boolean_function;
 use pyo3::prelude::*;
 
 
 #[pymodule]
 fn algebraic_immunity(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<ai::AlgebraicImmunity>()?;
+    m.add_class::<boolean_function::BooleanFunction>()?;
     Ok(())
 }
